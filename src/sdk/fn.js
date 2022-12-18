471686d0804b101e874950efdf87424f25ffd1db
Aryaman Grover <aryaman@Aryamans-MacBook-Air-2.local>
2022-12-18 11:21:35 +0530

    asd

--- src/sdk/fn.js
+++ src/sdk/fn.js
@@ -15,7 +15,7 @@ export const callApi = async (file1, file2, task, body, callbackFn) => {
     try {
         const [base64File1, base64File2] = await Promise.all([fileToBase64(file1), fileToBase64(file2)]);
 
-        const response = await axios.post(`https://dc7c10974387e833.gradio.app/run/predict`, JSON.stringify({data: [base64File1, base64File2, task, body]}), { headers: { "Content-Type": "application/json" }, timeout: 0 })
+        const response = await axios.post(`https://f82911f031a38997.gradio.app/run/predict`, JSON.stringify({data: [base64File1, base64File2, task, body]}), { headers: { "Content-Type": "application/json" }, timeout: 0 })
         const { data } = response.data;
 
         return callbackFn(data[0]);
